9c48ae3a9bfba95d4bec6136e942035999ad8a59
aakusuryawanshi1 <aakusuryawanshi1@gmail.com>
2025-11-26 14:44:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function toggleMenu() {
-    const nav = document.getElementById("nav-links");
-    nav.classList.toggle("show");
+function toggleMenu(){
+    document.getElementById("nav-links").classList.toggle("show");
 }
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 768){
+        document.getElementById("nav-links").classList.remove("show");
+    }
+});
